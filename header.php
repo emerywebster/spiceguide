4157274bdc2078fd06c6eb1d0aa66de6cc7f46b4
Webb Henderson <webb.henderson@gmail.com>
2016-02-22 11:41:29 -0700

    add ga

--- header.php
+++ header.php
@@ -6,6 +6,16 @@
 <meta name="p:domain_verify" content="12ee3ce83ed1e68760965a68c19f8e41"/>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-74159014-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 <?php wp_head(); ?>
 </head>
